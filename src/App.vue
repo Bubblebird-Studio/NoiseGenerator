0bbe8f8de8abfb96626b04e5439984d8d6ac4370
Fabien <fabien.weibel@gmail.com>
2025-07-17 17:44:30 +0200

    added channels options

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,26 @@
           <span class="m-3" v-if="is3d">{{ xTiles }} x {{ yTiles }} tiles</span>
         </p>
 
+        <div class="input-group mb-3">
+          <label class="input-group-text">Display</label>
+          <label class="input-group-text" for="outputR">R</label>
+          <div class="input-group-text">
+            <input class="form-check-input" type="checkbox" id="outputR" v-model="settings.channels[0].display">
+          </div>
+          <label class="input-group-text" for="outputG">G</label>
+          <div class="input-group-text">
+            <input class="form-check-input" type="checkbox" id="outputG" v-model="settings.channels[1].display">
+          </div>
+          <label class="input-group-text" for="outputB">B</label>
+          <div class="input-group-text">
+            <input class="form-check-input" type="checkbox" id="outputB" v-model="settings.channels[2].display">
+          </div>
+          <label class="input-group-text" for="outputA">A</label>
+          <div class="input-group-text">
+            <input class="form-check-input" type="checkbox" id="outputA" v-model="settings.channels[3].display">
+          </div>
+        </div>
+
         <div class="btn-group" role="group">
           <button class="btn btn-primary mt-3" type="button" @click="exportImage()"><i class="bi bi-download"></i> Export PNG</button>
           <button class="btn btn-secondary mt-3" type="button" @click="copyImageToClipboard()"><i class="bi bi-copy"></i> Copy</button>
@@ -243,16 +263,24 @@
             </span>
           </div>
 
-          <!-- <div v-else class="input-group mb-3">
-            <label class="input-group-text" for="normalScale">Normal scale</label>
+          <label class="">Gain - Gamma - offset</label>
+s
+          <div class="input-group mb-3">
             <div class="input-group-text">
-              <input type="range" class="form-range" min="0.0" max="2.0" step="0.01" v-model="settings.normalScale" @dblclick="settings.normalScale = defaultSettings.normalScale">
+              <input type="range" class="form-range" :style="{width: '60px'}" min="0.0" max="2.0" step="0.01" id="gain" v-model="activeChannel.gain" @dblclick="activeChannel.gain = 1.0">
             </div>
-            <label class="input-group-text" for="normalScale">{{ settings.normalScale }}</label>
-            <span class="input-group-text" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Controls the strength of the normal map.">
-              <i class="bi bi-question"></i>
-            </span>
-          </div> -->
+            <label class="input-group-text" for="gain">{{ activeChannel.gain }}</label>
+
+            <div class="input-group-text">
+              <input type="range" class="form-range" :style="{width: '60px'}" min="0.0" max="2.0" step="0.01" id="gamma" v-model="activeChannel.gamma" @dblclick="activeChannel.gamma = 1.0">
+            </div>
+            <label class="input-group-text" for="gamma">{{ activeChannel.gamma }}</label>
+
+            <div class="input-group-text">
+              <input type="range" class="form-range" :style="{width: '60px'}" min="-1.0" max="1.0" step="0.01" id="offset" v-model="activeChannel.offset" @dblclick="activeChannel.offset = 0.0">
+            </div>
+            <label class="input-group-text" for="offset">{{ activeChannel.offset }}</label>
+          </div>
 
           <div class="input-group mb-3">
             <label class="input-group-text" for="invert">Invert</label>
@@ -376,46 +404,46 @@ const defaultGeneratorSettings = {
 
 const channelsPresets = {
     "Default": [
-      { generator: 0, type: 0, invert: false },
-      { generator: 0, type: 0, invert: false },
-      { generator: 0, type: 0, invert: false },
-      { generator: 0, type: 4, invert: false },
+      { display: true, generator: 0, type: 0, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 0, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 0, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 4, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
     ],
     "Each generator its channel": [
-      { generator: 0, type: 0, invert: false },
-      { generator: 0, type: 3, invert: false },
-      { generator: 0, type: 3, invert: false },
-      { generator: 0, type: 4, invert: false },
+      { display: true, generator: 0, type: 0, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 3, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 3, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 4, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
     ],
     "Generator 0 to Normal map (OpenGL)": [
-      { generator: 0, type: 1, invert: false },
-      { generator: 0, type: 2, invert: false },
-      { generator: 0, type: 4, invert: false },
-      { generator: 0, type: 4, invert: false },
+      { display: true, generator: 0, type: 1, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 2, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 4, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 4, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
     ],
     "Generator 0 to Normal map (DirectX)": [
-      { generator: 0, type: 1, invert: false },
-      { generator: 0, type: 2, invert: true },
-      { generator: 0, type: 4, invert: false },
-      { generator: 0, type: 4, invert: false },
+      { display: true, generator: 0, type: 1, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 2, invert: true , gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 4, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 4, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
     ],
     "Generator 0 to red channel": [
-      { generator: 0, type: 0, invert: false },
-      { generator: 0, type: 3, invert: false },
-      { generator: 0, type: 3, invert: false },
-      { generator: 0, type: 4, invert: false },
+      { display: true, generator: 0, type: 0, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 3, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 3, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 4, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
     ],
     "Generator 1 to green channel": [
-      { generator: 0, type: 3, invert: false },
-      { generator: 1, type: 0, invert: false },
-      { generator: 0, type: 3, invert: false },
-      { generator: 0, type: 4, invert: false },
+      { display: true, generator: 0, type: 3, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 1, type: 0, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 3, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 4, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
     ],
     "Generator 2 to blue channel": [
-      { generator: 0, type: 3, invert: false },
-      { generator: 0, type: 3, invert: false },
-      { generator: 2, type: 0, invert: false },
-      { generator: 0, type: 4, invert: false },
+      { display: true, generator: 0, type: 3, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 3, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 2, type: 0, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
+      { display: true, generator: 0, type: 4, invert: false, gain: 1.0, gamma: 1.0, offset: 0.0 },
     ],
 }
 
@@ -523,18 +551,34 @@ function GetUniformData(generator: number) {
     settings.generators[generator].voronoiDistanceType,
     settings.generators[generator].seamless ? 1 : 0,
     settings.generators[generator].seed,
+    settings.channels[0].display,
     settings.channels[0].generator,
     settings.channels[0].type,
     settings.channels[0].invert ? 1 : 0,
+    settings.channels[0].gain,
+    settings.channels[0].gamma,
+    settings.channels[0].offset,
+    settings.channels[1].display,
     settings.channels[1].generator,
     settings.channels[1].type,
     settings.channels[1].invert ? 1 : 0,
+    settings.channels[1].gain,
+    settings.channels[1].gamma,
+    settings.channels[1].offset,
+    settings.channels[2].display,
     settings.channels[2].generator,
     settings.channels[2].type,
     settings.channels[2].invert ? 1 : 0,
+    settings.channels[2].gain,
+    settings.channels[2].gamma,
+    settings.channels[2].offset,
+    settings.channels[3].display,
     settings.channels[3].generator,
     settings.channels[3].type,
     settings.channels[3].invert ? 1 : 0,
+    settings.channels[3].gain,
+    settings.channels[3].gamma,
+    settings.channels[3].offset,
   ]);
 }
 
--- src/compositingShader.wgsl
+++ src/compositingShader.wgsl
@@ -20,18 +20,34 @@ fn fragment(@builtin(position) fragCoord: float4) -> @location(0) float4 {
   let resolutionZ = uint(settings.resolutionZ);
   let xTiles = uint(settings.xTiles);
   let yTiles = uint(settings.yTiles);
+  let channel0display = float(settings.channel0display);
   let channel0generator = uint(settings.channel0generator);
   let channel0type = uint(settings.channel0type);
   let channel0invert = uint(settings.channel0invert) != 0u;
+  let channel0gain = float(settings.channel0gain);
+  let channel0gamma = float(settings.channel0gamma);
+  let channel0offset = float(settings.channel0offset);
+  let channel1display = float(settings.channel1display);
   let channel1generator = uint(settings.channel1generator);
   let channel1type = uint(settings.channel1type);
   let channel1invert = uint(settings.channel1invert) != 0u;
+  let channel1gain = float(settings.channel1gain);
+  let channel1gamma = float(settings.channel1gamma);
+  let channel1offset = float(settings.channel1offset);
+  let channel2display = float(settings.channel2display);
   let channel2generator = uint(settings.channel2generator);
   let channel2type = uint(settings.channel2type);
   let channel2invert = uint(settings.channel2invert) != 0u;
+  let channel2gain = float(settings.channel2gain);
+  let channel2gamma = float(settings.channel2gamma);
+  let channel2offset = float(settings.channel2offset);
+  let channel3display = float(settings.channel3display);
   let channel3generator = uint(settings.channel3generator);
   let channel3type = uint(settings.channel3type);
   let channel3invert = uint(settings.channel3invert) != 0u;
+  let channel3gain = float(settings.channel3gain);
+  let channel3gamma = float(settings.channel3gamma);
+  let channel3offset = float(settings.channel3offset);
   let normalScale = 1.0;
 
   let fragX = uint(fragCoord.x);
@@ -65,22 +81,22 @@ fn fragment(@builtin(position) fragCoord: float4) -> @location(0) float4 {
     values[i][4] = 1.0;
   }
   
-  var outputR = values[channel0generator][channel0type];
-  var outputG = values[channel1generator][channel1type];
-  var outputB = values[channel2generator][channel2type];
-  var outputA = values[channel3generator][channel3type];
+  var outputR = saturate(pow(values[channel0generator][channel0type] * channel0gain, channel0gamma) + channel0offset);
+  var outputG = saturate(pow(values[channel1generator][channel1type] * channel1gain, channel1gamma) + channel1offset);
+  var outputB = saturate(pow(values[channel2generator][channel2type] * channel2gain, channel2gamma) + channel2offset);
+  var outputA = saturate(pow(values[channel3generator][channel3type] * channel3gain, channel3gamma) + channel3offset);
 
   if (channel0invert) { outputR = 1.0 - outputR; };
   if (channel1invert) { outputG = 1.0 - outputG; };
-  if (channel2invert) { outputG = 1.0 - outputB; };
+  if (channel2invert) { outputB = 1.0 - outputB; };
   if (channel3invert) { outputA = 1.0 - outputA; };
 
-  let output = float4(outputR, outputG, outputB, outputA);
+  outputBuffer[outputIndex * 4 + 0] = outputR;
+  outputBuffer[outputIndex * 4 + 1] = outputG;
+  outputBuffer[outputIndex * 4 + 2] = outputB;
+  outputBuffer[outputIndex * 4 + 3] = outputA;
 
-  outputBuffer[outputIndex * 4 + 0] = output.r;
-  outputBuffer[outputIndex * 4 + 1] = output.g;
-  outputBuffer[outputIndex * 4 + 2] = output.b;
-  outputBuffer[outputIndex * 4 + 3] = output.a;
+  let output = float4(outputR * channel0display, outputG * channel1display, outputB * channel2display, mix(1.0, outputA, channel3display));
 
   let checkerColor = (floor(float(fragX) * 0.1) + floor(float(fragY) * 0.1)) % 2.0 * 0.2 + 0.2;
   let background = float4(checkerColor, checkerColor, checkerColor, checkerColor);
